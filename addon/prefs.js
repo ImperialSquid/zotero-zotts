f4364a9dd37c9b03c99e2caef23560ddd1fe72bb
Ben Jaeger <benjaeger99@gmail.com>
2023-04-21 11:55:25 +0100

    feat: add default prefs and checking code

--- addon/prefs.js
+++ addon/prefs.js
@@ -1,2 +1,5 @@
 pref("extensions.zotero.__addonRef__.enable", true);
 pref("extensions.zotero.__addonRef__.input", "This is input");
+pref("extensions.zotero.__addonRef__.voiceVolume", 1);
+pref("extensions.zotero.__addonRef__.voiceRate", 1);
+pref("extensions.zotero.__addonRef__.voicePitch", 1);
---
+++ src/modules/defaults.ts
@@ -0,0 +1,18 @@
+import {getPref, setPref} from "./prefUtils";
+
+export function setDefaultPrefs() {
+    let pitch = (getPref("pitch") as number) || -1
+    if (pitch < 0 || pitch > 2){  // constrain pitch to [0,2], as limited by spec
+        setPref("pitch", 1)
+    }
+
+    let rate = (getPref("rate") as number) || -1
+    if (rate < 1/4 || rate > 4){  // constrain rate to [0.25x, 4x], spec limits to [0.1x, 10x] but those are very wide
+        setPref("rate", 1)
+    }
+
+    let volume = (getPref("volume") as number) || -1
+    if (volume < 0 || volume > 1){  // constrain rate to [0, 1], as limited by spec
+        setPref("volume", 1)
+    }
+}
