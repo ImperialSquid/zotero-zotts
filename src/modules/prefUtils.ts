9caa62f9b0c9da4ec9ef27347c88313d6887f63e
Ben Jaeger <benjaeger99@gmail.com>
2023-04-22 22:35:47 +0100

    fix: standardise pref names between prefs.js and prefUtils.ts

--- src/modules/prefUtils.ts
+++ src/modules/prefUtils.ts
@@ -13,18 +13,18 @@ export function clearPref(key: string) {
 }
 
 export function setDefaultPrefs() {
-    let pitch = (getPref("pitch") as number) || -1
+    let pitch = (getPref("voicePitch") as number) || -1
     if (pitch < 0 || pitch > 2){  // constrain pitch to [0,2], as limited by spec
-        setPref("pitch", 1)
+        setPref("voicePitch", 1)
     }
 
-    let rate = (getPref("rate") as number) || -1
+    let rate = (getPref("voiceRate") as number) || -1
     if (rate < 1/4 || rate > 4){  // constrain rate to [0.25x, 4x], spec limits to [0.1x, 10x] but those are very wide
-        setPref("rate", 1)
+        setPref("voiceRate", 1)
     }
 
-    let volume = (getPref("volume") as number) || -1
+    let volume = (getPref("voiceVolume") as number) || -1
     if (volume < 0 || volume > 1){  // constrain rate to [0, 1], as limited by spec
-        setPref("volume", 1)
+        setPref("voiceVolume", 1)
     }
 }
