e4b10b5dc73019cce68c82d4418cca84408026ed
Ben Jaeger <benjaeger99@gmail.com>
2023-04-27 08:36:40 +0100

    feat: add ttsWebSpeech.ts for speech synth through WebSpeech API

---
+++ src/modules/tts/ttsWebSpeech.ts
@@ -0,0 +1,61 @@
+import {ttsBase} from "./ttsBase";
+
+import {BasicTool} from "zotero-plugin-toolkit/dist/basic";
+
+const bt = new BasicTool();
+const synthesis = bt.getGlobal("window").speechSynthesis;
+const utterance = bt.getGlobal("window").SpeechSynthesisUtterance;
+
+export class ttsWebSpeech extends ttsBase {
+    protected canPause: boolean;
+    protected voices: Array<string>;
+    private voice: string;
+    private paused: boolean = false;
+
+    constructor() {
+        super();
+        this.canPause = true;
+        this.voices = this.getVoices();
+        this.voice = this.voices[0];
+    }
+
+    speak(input: string): void {
+        this.stop() // prevent queueing multiple utterances for simplicity
+
+        const utt = new utterance(input);
+        utt.rate = this.getRate();
+        utt.pitch = this.getPitch();
+        utt.volume = this.getVolume();
+        utt.voice = this.getVoiceByName(this.voice);
+
+        synthesis.speak(utt);
+    }
+
+    stop(): void {
+        synthesis.cancel();
+    }
+
+    pause(): void {
+        synthesis.pause();
+        this.paused = true;
+    }
+
+    resume(): void {
+        synthesis.resume();
+        this.paused = false;
+    }
+
+    getVoices(): Array<string> {
+        return synthesis.getVoices().map((v) => `${v.name} (${v.lang})`)
+    }
+
+    setVoice(newVoice: string): void {
+        if (this.voices.indexOf(newVoice) > -1) {
+            this.voice = newVoice;
+        }
+    }
+
+    private getVoiceByName(voice: string): SpeechSynthesisVoice {
+        return synthesis.getVoices().filter((v) => `${v.name} (${v.lang})` === voice)[0];
+    }
+}
