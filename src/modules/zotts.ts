95632bdef8b2769c938114a0a5666b292dceed51
Ben Jaeger <benjaeger99@gmail.com>
2023-04-28 11:09:46 +0100

    chore: use global zToolkit

--- src/modules/zotts.ts
+++ src/modules/zotts.ts
@@ -1,16 +1,12 @@
-import {ZoteroToolkit} from "zotero-plugin-toolkit"
 import {ttsBase} from "./tts/ttsBase";
 import {ttsWebSpeech} from "./tts/ttsWebSpeech";
 import {ttsSay} from "./tts/ttsSay";
 
 export class ZoTTS {
-    private ztk: ZoteroToolkit;
     private tts: ttsBase;
 
     constructor() {
-        this.ztk = new ZoteroToolkit()
-
-        if (this.ztk.isZotero7()) {
+        if (ztoolkit.isZotero7()) {
             this.tts = new ttsWebSpeech()
         } else {
             this.tts = new ttsSay()
