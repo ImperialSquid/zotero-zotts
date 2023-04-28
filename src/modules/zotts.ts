280d4097be02172261f3ad549eb53aafc95b9bca
Ben Jaeger <benjaeger99@gmail.com>
2023-04-28 11:40:37 +0100

    feat: add funcs for adding/removing notifiers

--- src/modules/zotts.ts
+++ src/modules/zotts.ts
@@ -11,16 +11,37 @@ export class ZoTTS {
         } else {
             this.tts = new ttsSay()
         }
-
-        this.addButton()
     }
 
-    private addButton() {
-        this.ztk.ReaderInstance.register("initialized", "zotts",
-            async (reader) => {
-                await reader._initPromise
-                await reader._waitForReader()
-                this.ztk.log(`ZoTTS reader id ${reader.itemID} initialized`)
-            })
+    private static registerNotifier(func: Function){
+        const callback = {
+            notify: async (
+                event: string,
+                type: string,
+                ids: number[] | string[],
+                extraData: { [key: string]: any }
+            ) => {
+                if (!addon?.data.alive) {
+                    ZoTTS.unregisterNotifier(notifierID);
+                    return;
+                }
+                func(event, type, ids, extraData);
+            },
+        };
+
+        const notifierID = ztoolkit.getGlobal("Zotero").Notifier.registerObserver(callback,
+            ["tab","item","file",]
+        );
+
+        // Unregister callback when the window closes (important to avoid a memory leak)
+        window.addEventListener(
+            "unload",
+            (e: Event) => { ZoTTS.unregisterNotifier(notifierID); },
+            false
+        );
+    }
+
+    private static unregisterNotifier(notifierID: string) {
+        ztoolkit.getGlobal("Zotero").Notifier.unregisterObserver(notifierID);
     }
 }
