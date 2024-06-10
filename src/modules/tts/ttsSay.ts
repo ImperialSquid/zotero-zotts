ab8570d72310d3eea56d9df0cbe953da746d3ee3
Ben Jaeger <benjaeger99@gmail.com>
2024-06-10 02:55:05 +0100

    refactor: split out OS native tts into three classes for cleanness

--- src/modules/tts/ttsSay.ts
+++ src/modules/tts/ttsSay.ts
@@ -1,72 +1,118 @@
-import {ttsBase} from "./ttsBase";
-const say = require("say");
+import {ttsEngineBase, ttsMixin} from "./ttsBase";
 
 const VOICES_UNAVAILABLE = "Voice selection unavailable";
 
-export class ttsSay extends ttsBase{
-    protected canPause: boolean;
-    protected voices: Array<string>;
-    private voice: string;
-    // macos has a lot of voices but only some are appropriate for any particular language
-    // this is a dictionary of names with language codes (for display) to voice names (for tts)
-    private voiceDict: {[key: string]: string} = {};
-    private say: typeof say;
+export class ttsWindows extends ttsMixin implements ttsEngineBase {
+    canPause: boolean;
 
     constructor() {
         super();
         this.canPause = false;
-
-        this.getVoiceDict()
-
-        this.voices = this.getVoices();
-        this.voice = this.voices[0];
-
-        this.say = say.Say(undefined);
     }
 
     getVoices(): Array<string> {
-        return Object.keys(this.voiceDict);
+        throw new Error("Not implemented");
     }
 
-    setVoice(newVoice: string): void {
-        if (this.voiceDict[newVoice]) {
-            this.voice = newVoice;
-        }
+    setVoice(newVoices: string): void {
+        throw new Error("Not implemented");
     }
 
     speak(input: string): void {
-        this.say.speak(input, this.voice, this.rate);
+        throw new Error("Not implemented");
     }
 
     stop(): void {
-        this.say.stop();
-    }
-
-    private async getVoiceDict(): Promise<void> {
-        let [voices] = await Promise.all([say.getInstalledVoices()]);
-
-        const voiceDict: {[key: string]: string} = {};
-
-        if (say.platform === "darwin") {
-            const regex = /([A-Za-z]*)\s\(.*\)/;
-            for (const voice of voices) {
-                const match = voice.match(regex);  // extract "Alex (en_US)" -> "Alex"
-                if (match) {
-                    voiceDict[voice] = match[1];
-                }
-            }
-        } else if (say.platform === "win32") {
-            for (const voice of voices) {
-                voiceDict[voice] = voice;
-            }
-        } else if (say.platform === "linux") {
-            // linux get voices not currently supported (getInstalledVoices will throw an error)
-            // code will go here eventually
-            voiceDict[VOICES_UNAVAILABLE] = "";
-        } else {
-            voiceDict[VOICES_UNAVAILABLE] = "";
-        }
-
-        this.voiceDict = voiceDict;
+        throw new Error("Not implemented");
     }
 }
+
+export class ttsMacOS extends ttsMixin implements ttsEngineBase {
+    canPause: boolean;
+
+    constructor() {
+        super();
+        this.canPause = false;
+    }
+
+    getVoices(): Array<string> {
+        throw new Error("Not implemented");
+    }
+
+    setVoice(newVoices: string): void {
+        throw new Error("Not implemented");
+    }
+
+    speak(input: string): void {
+        throw new Error("Not implemented");
+    }
+
+    stop(): void {
+        throw new Error("Not implemented");
+    }
+}
+
+export class ttsLinux extends ttsMixin implements ttsEngineBase {
+    canPause: boolean;
+
+    constructor() {
+        super();
+        this.canPause = false;
+    }
+
+    getVoices(): Array<string> {
+        throw new Error("Not implemented");
+    }
+
+    setVoice(newVoices: string): void {
+        throw new Error("Not implemented");
+    }
+
+    speak(input: string): void {
+        throw new Error("Not implemented");
+    }
+
+    stop(): void {
+        throw new Error("Not implemented");
+    }
+}
+
+// Future code snippets for getting output from a command line process, taken from zotero scaffold.js
+//
+// let outputFile = OS.Path.join(
+//     Zotero.getTempDirectory().path,
+//     `lint_output_${Zotero.Utilities.randomString()}.txt`
+// );
+//
+// // Create a file object for the executable
+// const file = Components.classes["@mozilla.org/file/local;1"]
+//     .createInstance(Components.interfaces.nsIFile);
+// file.initWithPath("C:\\Windows\\System32\\cmd.exe");
+//
+// // Create a process object for the executable
+// const process = Components.classes["@mozilla.org/process/util;1"]
+//     .createInstance(Components.interfaces.nsIProcess);
+// process.init(file);
+//
+// // Set the arguments for the command line
+// const args = ["/c", "cd", ">", outputFile];
+//
+// // Create an observer object for the process
+// const observer = {
+//     observe: function (subject: typeof process, topic: string, data: any) {
+//         if (topic == "process-finished") {
+//             // The process has finished
+//             var exitval = process.exitValue; // Get the exit value
+//             ztoolkit.log("The process exited with code " + exitval);
+//             ztoolkit.log("The process output is " + data);
+//             ztoolkit.log("The process output is " + subject);
+//             // Your callback here
+//         } else if (topic == "process-failed") {
+//             // The process failed to launch or was killed
+//             ztoolkit.log("The process failed");
+//         }
+//     }
+// };
+//
+// // Run the process asynchronously and pass the observer
+// process.runAsync(args, args.length, observer);
