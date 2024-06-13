8d995a8a050ce8bd798634a4da1beb8b59aa8ef5
Ben Jaeger <benjaeger99@gmail.com>
2024-06-13 01:28:09 +0100

    chore: comment out os command line tts engines

--- src/modules/tts/manager.ts
+++ src/modules/tts/manager.ts
@@ -1,6 +1,6 @@
 import {ttsEngineBase} from "./base";
 import {webSpeech} from "./webSpeech";
-import {ttsWindows, ttsLinux, ttsMacOS} from "./native";
+// import {ttsWindows, ttsLinux, ttsMacOS} from "./native";
 
 export class manager {
     private engines: Array<ttsEngineBase> = [];
@@ -14,12 +14,12 @@ export class manager {
             this.engines.push(new webSpeech());
         }
 
-        if (Zotero.isWindows()) {
-            this.engines.push(new ttsWindows());
-        } else if (Zotero.Zotero.isMacOS()) {
-            this.engines.push(new ttsMacOS());
-        } else if (Zotero.isLinux()) {
-            this.engines.push(new ttsLinux());
-        }
+        // if (Zotero.isWindows()) {
+        //     this.engines.push(new ttsWindows());
+        // } else if (Zotero.Zotero.isMacOS()) {
+        //     this.engines.push(new ttsMacOS());
+        // } else if (Zotero.isLinux()) {
+        //     this.engines.push(new ttsLinux());
+        // }
     }
 }
