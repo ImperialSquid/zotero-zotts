a56dee218a2014b401cef8fae8c50c177eab0ff9
Ben Jaeger <benjaeger99@gmail.com>
2024-06-24 20:42:23 +0100

    feat: add "read abstract" to menu list

--- src/modules/menu.ts
+++ src/modules/menu.ts
@@ -12,11 +12,23 @@ export function registerMenu() {
         // TODO: l10n - implement locale
         label: "Read Title",
         commandListener: (ev) => {
-            // TODO: implement queueing?
             let item = Zotero.Items.get(ZoteroPane.getSelectedItems(true)[0])
             let title = item.getField("title") as string;
             addon.hooks.onSpeak(title);
         },
+        // TODO: prettify - add icon
+        // icon: menuIcon,
+    });
+    ztoolkit.Menu.register("item", {
+        tag: "menuitem",
+        // TODO: l10n - implement locale
+        label: "Read Abstract",
+        commandListener: (ev) => {
+            let item = Zotero.Items.get(ZoteroPane.getSelectedItems(true)[0])
+            let title = item.getField("abstractNote") as string;
+            addon.hooks.onSpeak(title);
+        },
+        // TODO: prettify - add icon
         // icon: menuIcon,
     });
 }
