51d0e3384a7dd727d82a7304772f8ec454e0c9ef
Ben Jaeger <benjaeger99@gmail.com>
2024-07-19 03:34:17 +0100

    wip: begin adding shortcuts

--- src/modules/shortcuts.ts
+++ src/modules/shortcuts.ts
@@ -1,3 +1,12 @@
 export function registerShortcuts() {
-    null
+    // TODO: prefs - add main key to settings
+    //   S for Speech is nice but maybe not desirable for user
+    // TODO: prefs - implement toggle between title/abstract and annotation/comment reading
+    ztoolkit.Keyboard.register((ev, data) => {
+        if (data.type === "keyup") {
+            if (ev.ctrlKey && ev.key === "s") {
+                addon.hooks.onContextualSpeak()
+            }
+        }
+    })
 }
