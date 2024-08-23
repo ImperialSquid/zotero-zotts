44460655c1c46650a59b705b5038f9398830c1e0
Ben Jaeger <benjaeger99@gmail.com>
2024-08-23 01:15:55 +0100

    feat: implement getFullText to retrieve ful text of a paper

--- src/modules/utils/readerUtils.ts
+++ src/modules/utils/readerUtils.ts
@@ -4,6 +4,10 @@ export function getSelectedText(reader: ReaderInstance) {
     return ztoolkit.Reader.getSelectedText(reader)
 }
 
+export async function getFullText(reader: ReaderInstance) {
+    return await Zotero.Items.get(reader.itemID ?? "").attachmentText
+}
+
 export function getSelectedAnnotations(reader: ReaderInstance) {
     let annos = reader._internalReader._annotationManager._annotations
     return annos.filter((anno) => reader._internalReader._state.selectedAnnotationIDs.includes(anno.id))
