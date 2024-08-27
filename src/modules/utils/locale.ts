89d83ef0a7d919847b69c364b8de7b7cabf9779b
Ben Jaeger <benjaeger99@gmail.com>
2024-08-27 04:05:44 +0100

    fix: locale should load all .ftl files

--- src/modules/utils/locale.ts
+++ src/modules/utils/locale.ts
@@ -10,7 +10,10 @@ function initLocale() {
       typeof Localization === "undefined"
           ? ztoolkit.getGlobal("Localization")
           : Localization
-  )([`${config.addonRef}-addon.ftl`], true)
+  )([
+    `${config.addonRef}-addon.ftl`,
+    `${config.addonRef}-preferences.ftl`,
+  ], true)
 
   addon.data.locale = {
     current: l10n,
