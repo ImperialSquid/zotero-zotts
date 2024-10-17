a4c1eede95b401ec4b24d9f22c79215703ff1f3c
Ben Jaeger <benjaeger99@gmail.com>
2024-10-17 03:57:54 +0100

    feat: add favourites to prefs.js
    I'm not a huuuge fan of Ctrl+Shift+Q to cycle favourites, but a lot of the better keys have been taken lol, it's up to the user to pick a better option

--- addon/prefs.js
+++ addon/prefs.js
@@ -8,9 +8,11 @@ pref("__prefsPrefix__.webSpeech.volume", 50);
 pref("__prefsPrefix__.shortcuts.speak", "S");
 pref("__prefsPrefix__.shortcuts.pause", "P");
 pref("__prefsPrefix__.shortcuts.cancel", "C");
+pref("__prefsPrefix__.shortcuts.cycleFavourite", "Q")
 pref("__prefsPrefix__.shortcuts.swapLibraryItem", "false");
 pref("__prefsPrefix__.shortcuts.swapAnnotation", "false");
 pref("__prefsPrefix__.subs.removeParenCitations", false)
 pref("__prefsPrefix__.subs.removeNumericCitations", false)
 pref("__prefsPrefix__.subs.removeEmails", false)
 pref("__prefsPrefix__.subs.customSubs", "")
+pref("__prefsPrefix__.favouritesList", "[]");
