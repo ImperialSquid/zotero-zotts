06c4b2832bcc8260979671f308d3eac3c35cca43
Ben Jaeger <benjaeger99@gmail.com>
2024-10-17 03:37:20 +0100

    wip: add skeletons for favourites.ts functions

---
+++ src/modules/favourites.ts
@@ -0,0 +1,27 @@
+import { getPref, setPref } from "./utils/prefs";
+
+function cycleFavourites() {
+}
+
+function addFavourite() {
+}
+
+function removeFavourite(
+    favToRemove: { [key: string]: string | number | boolean }
+) {
+}
+
+function constructFav() {
+}
+
+function compareFav(
+    arg1: {[key: string]: string | number | boolean},
+    arg2: {[key: string]: string | number | boolean}
+): boolean {
+}
+
+export {
+    cycleFavourites,
+    addFavourite,
+    removeFavourite
+}
