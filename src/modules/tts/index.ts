e39f3ba3413a23901bac52ce3d3e6f838a0c0511
Ben Jaeger <benjaeger99@gmail.com>
2025-03-06 17:07:30 +0000

    chore: refactor src/modules/tts/index.ts

--- src/modules/tts/index.ts
+++ src/modules/tts/index.ts
@@ -1,42 +1,42 @@
 import Addon from "../../addon"
 import { getString } from "../utils/locale";
 
-export async function initEngines(addon: Addon) {
+async function initEngines(addon: Addon) {
     addon.data.tts.current = "webSpeech"
 
     let wsaPromise = import("./webSpeech").then(
-        (e) => {
-            e.setDefaultPrefs()
+      (e) => {
+          e.setDefaultPrefs()
 
-            addon.data.tts.engines["webSpeech"] = {
-                name: "Web Speech API",
-                status: "loading",
-                speak: e.speak,
-                stop: e.stop,
-                canPause: true,
-                pause: e.pause,
-                resume: e.resume,
-                extras: {
-                    // technically not needed here since populateVoiceList exists, but could be useful
-                    getVoices: e.getVoices,
-                    populateVoiceList: e.populateVoiceList
-                }
-            }
+          addon.data.tts.engines["webSpeech"] = {
+              name: "Web Speech API",
+              status: "loading",
+              speak: e.speak,
+              stop: e.stop,
+              canPause: true,
+              pause: e.pause,
+              resume: e.resume,
+              extras: {
+                  // technically not needed here since populateVoiceList exists, but could be useful
+                  getVoices: e.getVoices,
+                  populateVoiceList: e.populateVoiceList
+              }
+          }
 
-            return e
-        }
+          return e
+      }
     ).then(
-        async (e) => {
-            // ztoolkit.log("WSA Initing")
-            await e.initEngine()
-            // ztoolkit.log("WSA init success")
-            addon.data.tts.engines["webSpeech"].status = "ready"
-        }
+      async (e) => {
+          // ztoolkit.log("WSA Initing")
+          await e.initEngine()
+          // ztoolkit.log("WSA init success")
+          addon.data.tts.engines["webSpeech"].status = "ready"
+      }
     ).catch(
-        (e) => {
-            // ztoolkit.log(`WSA init fail - ${e}`)
-            addon.data.tts.engines["webSpeech"].status = "error"
-        }
+      (e) => {
+          // ztoolkit.log(`WSA init fail - ${e}`)
+          addon.data.tts.engines["webSpeech"].status = "error"
+      }
     )
 
     // TODO: future - implement more engines
@@ -56,12 +56,12 @@ export async function initEngines(addon: Addon) {
     }
 }
 
-export function checkStatus() {
+function checkStatus() {
     return addon.data.tts.status === "ready"
-        && addon.data.tts.engines[addon.data.tts.current].status === "ready"
+      && addon.data.tts.engines[addon.data.tts.current].status === "ready"
 }
 
-export function reportStatus() {
+function reportStatus() {
     const popup = new Zotero.ProgressWindow({closeOnClick: true})
     popup.changeHeadline("ZoTTS")
 
@@ -79,12 +79,12 @@ export function reportStatus() {
         icon = ""  // TODO: UI - Add notice SVG
         timer = 5000
     } else if (addon.data.tts.engines[addon.data.tts.current]?.status === "error") {
-        texts.push(getString("status-addonError",  {
+        texts.push(getString("status-addonError", {
             args: {
                 engineName: addon.data.tts.engines[addon.data.tts.current]?.name
             }
         }))
-        texts.push(getString("status-errorCause",  {
+        texts.push(getString("status-errorCause", {
             args: {
                 engine: addon.data.tts.current,
                 cause: addon.data.tts.engines[addon.data.tts.current]?.errorMsg
@@ -93,7 +93,7 @@ export function reportStatus() {
         icon = ""  // TODO: UI - Add error SVG
         timer = 10000
     } else if (addon.data.tts.engines[addon.data.tts.current]?.status === "loading") {
-        texts.push(getString("status-addonLoading",  {
+        texts.push(getString("status-addonLoading", {
             args: {
                 engineName: addon.data.tts.engines[addon.data.tts.current]?.name
             }
@@ -101,7 +101,7 @@ export function reportStatus() {
         icon = ""  // TODO: UI - Add notice SVG
         timer = 5000
     } else if (addon.data.tts.status === "ready" &&
-            addon.data.tts.engines[addon.data.tts.current]?.status === "ready") {
+      addon.data.tts.engines[addon.data.tts.current]?.status === "ready") {
         texts.push(getString("status-allGood"))
         icon = "" // TODO: UI - Add ready SVG
         timer = 1500
@@ -109,8 +109,16 @@ export function reportStatus() {
 
     popup.addLines(texts.shift() ?? "", icon)  // first line gets the icon
 
-    texts.forEach((text) => {popup.addLines(text, "")})  // any remaining lines don't
+    texts.forEach((text) => {
+        popup.addLines(text, "")
+    })  // any remaining lines don't
 
     popup.show()
     popup.startCloseTimer(timer)
+}
+
+export {
+    initEngines,
+    checkStatus,
+    reportStatus,
 }
