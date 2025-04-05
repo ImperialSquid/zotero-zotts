944b526a87f123db0ba38cfef3acf9882e547835
Ben Jaeger <benjaeger99@gmail.com>
2025-04-06 00:57:01 +0100

    feat: implement getting text from selection to end

--- addon/locale/en-US/addon.ftl
+++ addon/locale/en-US/addon.ftl
@@ -11,6 +11,7 @@ anno-comment = Comm.
 speak-testVoice = This is an example sentence using the current voice and other settings.
 
 ## Popups
+## Addon/Engine Status Popups
 popup-allGood = Loaded and initialised successfully
 
 popup-addonLoadingTitle = NOTICE: ZoTTS is still loading
@@ -25,3 +26,14 @@ popup-engineErrorCause = Cause: { zotts-ttsEngine-errorCause }
 
 popup-loadingTrailer = Please be patient or submit a bug report if error persists
 popup-errorTrailer = Please submit a bug report
+
+## Speak From Here Popups
+popup-SFH-noSelection = Cannot use "Speak From Here" without selecting start point
+
+popup-SFH-unknownSelection1 = Couldn't find the selected text within full text of paper.
+popup-SFH-unknownSelection2 =
+    This is usually caused by selecting an equation/image/etc.
+    Please select more "standard" text to start from.
+
+popup-SFH-nonspecificSelection1 = Start point for "Speak From Here" is non-unique.
+popup-SFH-nonspecificSelection2 = Please select more text to start from.
--- src/modules/utils/readerUtils.ts
+++ src/modules/utils/readerUtils.ts
@@ -1,7 +1,55 @@
 import ReaderInstance = _ZoteroTypes.ReaderInstance;
+import { notifyGeneric } from "./notify";
+import { getString } from "./locale";
 
 export function getSelectedText(reader: ReaderInstance) {
     return ztoolkit.Reader.getSelectedText(reader)
+}
+
+export async function getSelectedTextToEnd(reader: ReaderInstance) {
+    const selected = getSelectedText(reader)
+
+    if (selected === "") {
+        // cannot "read from here" without a here to read from
+        notifyGeneric(
+          [getString("popup-SFH-noSelection")],
+          "info"
+        )
+
+        return ""
+    }
+
+    const full = await getFullText(reader)
+
+    const parts = full.split(selected)
+
+    if (parts.length < 2) {
+        // "read from here" failed to find the selected text
+        notifyGeneric(
+          [
+              getString("popup-SFH-unknownSelection1"),
+              getString("popup-SFH-unknownSelection2")
+          ],
+          "info"
+        )
+
+        return ""
+    }
+    if (parts.length > 2) {
+        // cannot "read from here" without a more specific start point
+        notifyGeneric(
+          [
+              getString("popup-SFH-nonspecificSelection1"),
+              getString("popup-SFH-nonspecificSelection2")
+          ],
+          "info"
+        )
+
+        return ""
+    }
+
+    // recombine separator with "from here" and return
+    return selected + parts[1]
 }
 
 export async function getFullText(reader: ReaderInstance) {
